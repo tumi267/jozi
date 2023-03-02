0b30e5cf124be9ccef9d264b7489362eda9e9735
tumi267 <itu.matlala4@gmail.com>
2023-03-02 17:27:03 +0200

    deployed web

--- src/Nav-Menu/Nav_Menu.js
+++ src/Nav-Menu/Nav_Menu.js
@@ -10,7 +10,7 @@ function Nav_Menu() {
     <div className='nav_menu'>
     <div  className='nav_menu_list'>
     <CloseIcon className='close_menu' onClick={()=>{close_menu()}}/>
-    <Link to='/' className='nav-links' onClick={()=>{close_menu()}}>home</Link>
+    <Link to='/jozi' className='nav-links' onClick={()=>{close_menu()}}>home</Link>
     <Link to='/Service' className='nav-links' onClick={()=>{close_menu()}}>services</Link>
     <Link to='/About' className='nav-links' onClick={()=>{close_menu()}}>about us</Link>
     <Link to='/Contact' className='nav-links' onClick={()=>{close_menu()}}>contact us</Link>
