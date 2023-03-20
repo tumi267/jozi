a26620809b396d30e97b8f9bc69c5c50181c331a
tumi267 <itu.matlala4@gmail.com>
2023-03-20 14:38:51 +0200

    update borweser router to work

--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import  {Routes,Route,HashRouter,} from 'react-router-dom'
+import Home from './Home/Home';
+import Nav from './Nav';
+import About from './About/About';
+import Service from './Ofers/Service';
+import Contact from './Contact/Contact';
+import Copy from './Copy';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+  <HashRouter>
+  <Nav/>
+  <Routes>
+  <Route path="/*" element={<Home/>}/>
+  <Route path="/jozi/About" element={<About/>}/>
+  <Route path="/jozi/Service" element={<Service/>}/>
+  <Route path="/jozi/Contact" element={<Contact/>}/>
+  </Routes>
+  <Copy/>
+  </HashRouter> 
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
